5c63a05574f1f3887bfa455d2caeba89977f374b
Beansak <isak.klaesson2002@gmail.com>
2023-04-14 14:00:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ gripper_motor = Motor(Port.A)
 elbow_motor.control.limits(speed=120, acceleration=120)
 base_motor.control.limits(speed=120, acceleration=120)
 
-
+gripper_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
+gripper_motor.reset_angle(0)
+gripper_motor.run_target(200, -90)
 
