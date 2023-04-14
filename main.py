9eac9bfe24748e66c750fe55f8b9413831c37fae
Beansak <isak.klaesson2002@gmail.com>
2023-04-14 14:12:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,29 @@ ev3 = EV3Brick()
 
 gripper_motor = Motor(Port.A)
 
+elbow_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
+
+base_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
+
+# Move motors
 
 elbow_motor.control.limits(speed=120, acceleration=120)
 base_motor.control.limits(speed=120, acceleration=120)
+
+# Close gripper -- Open Gripper 
 
 gripper_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
 gripper_motor.reset_angle(0)
 gripper_motor.run_target(200, -90)
 
+# Lower elbow
+
+elbow_motor.run_target(60, -40)
+
+#Close gripper
+
+gripper_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
+
+#Raise elbow
+
+elbow_motor.control.limits(speed=120, acceleration=120)
